5f60790a2fdc59886bbc16142e1373758a3bc997
Daniel Felipe Zamora Ortiz <d4n7elfelipe@gmail.com>
2020-09-16 18:13:38 -0500

    Updates

---
+++ precache-manifest.e5206defc18fbee7d1fe94823074b833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cebfaf7ea60ef081f1e4dbfe53cf225",
+    "url": "/github-test-react/index.html"
+  },
+  {
+    "revision": "893f6bc2ca6f333cc593",
+    "url": "/github-test-react/static/css/main.12c253eb.chunk.css"
+  },
+  {
+    "revision": "aeb099ad9cdd2de3585a",
+    "url": "/github-test-react/static/js/2.3079f3f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-test-react/static/js/2.3079f3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893f6bc2ca6f333cc593",
+    "url": "/github-test-react/static/js/main.9a3d6b39.chunk.js"
+  },
+  {
+    "revision": "162d58af53e33eae01ab",
+    "url": "/github-test-react/static/js/runtime-main.38abb8b7.js"
+  }
+]);
